e317bf56f42e1f1f0b15668322cdb4b385b6c980
Ming Wang <132119331+Ming-Wang9@users.noreply.github.com>
2026-06-09 23:14:02 -0400

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rowset = [set() for _ in range(9)]
+        colset = [set() for _ in range(9)]
+        sqaureset = [set() for _ in range(9)]
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == '.':
+                    continue
+                idx = r//3*3+c//3
+                if val in rowset[r] or val in colset[c] or val in sqaureset[idx]:
+                    return False
+                rowset[r].add(val)
+                colset[c].add(val)
+                sqaureset[idx].add(val)
+        return True
