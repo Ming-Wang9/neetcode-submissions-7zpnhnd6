7b55118189a143ac8f577453af831803524a0f2b
Ming Wang <132119331+Ming-Wang9@users.noreply.github.com>
2026-07-09 00:41:58 -0400

    Add: subsets - submission-4

---
+++ Data Structures & Algorithms/subsets/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        def backtrack(i,sub):
+            if i == len(nums):
+                self.res.append(sub.copy())
+                return
+            sub.append(nums[i])
+            backtrack(i+1,sub)
+            sub.pop()
+            backtrack(i+1,sub)
+        backtrack(0,[])
+        return self.res
