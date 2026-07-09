7c01263c0996888f909fbfc87ee88e9764efafb5
Ming Wang <132119331+Ming-Wang9@users.noreply.github.com>
2026-07-09 00:48:45 -0400

    Add: permutations - submission-8

---
+++ Data Structures & Algorithms/permutations/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        self.res = []
+        def backtrack(sub):
+            if len(sub) == len(nums):
+                self.res.append(sub.copy())
+                return 
+            for n in nums:
+                if n not in sub:
+                    sub.append(n)
+                    backtrack(sub)
+                    sub.pop()
+        backtrack([])
+        return self.res
